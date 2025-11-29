88f9e0201b46a900bfe90449261c9d3c792999a4
twtest <tw@test.com>
2025-11-29 10:58:48 -0500

    ch06_l04 completed

---
+++ ch_06_data_structures_intro/l04_lists.py
@@ -0,0 +1,5 @@
+def last_work_experience(work_experiences):
+    if not work_experiences:
+        return None
+    # return work_experiences[len(work_experiences) - 1]
+    return work_experiences[-1]

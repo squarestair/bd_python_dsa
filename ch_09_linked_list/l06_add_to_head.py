da49ea224b81d340e9eb9178b2f6a301de7ceb3b
twtest <tw@test.com>
2025-11-30 22:29:10 -0500

    ch09_l06 completed

---
+++ ch_09_linked_list/l06_add_to_head.py
@@ -0,0 +1,36 @@
+from l06_node import Node
+
+
+class LinkedList:
+    def add_to_head(self, node):
+        if self.head is None:
+            self.head = node
+            return
+        node.next = self.head
+        self.head = node
+
+    # don't touch below this line
+
+    def add_to_tail(self, node):
+        if self.head is None:
+            self.head = node
+            return
+        last_node = None
+        for current_node in self:
+            last_node = current_node
+        last_node.set_next(node)
+
+    def __init__(self):
+        self.head = None
+
+    def __iter__(self):
+        node = self.head
+        while node is not None:
+            yield node
+            node = node.next
+
+    def __repr__(self):
+        nodes = []
+        for node in self:
+            nodes.append(node.val)
+        return " -> ".join(nodes)

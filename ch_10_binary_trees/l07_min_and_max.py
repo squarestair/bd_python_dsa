82ae50d74ee3dab9ead2c0919258f41b8f97fa1f
tw <tw@test.com>
2025-12-01 21:45:07 -0500

    ch10_l07 completed

---
+++ ch_10_binary_trees/l07_min_and_max.py
@@ -0,0 +1,37 @@
+class BSTNode:
+    def get_min(self):
+        if self.left:
+            return self.left.get_min()
+        return self.val
+
+    def get_max(self):
+        if self.right:
+            return self.right.get_max()
+        return self.val
+
+    # don't touch below this line
+
+    def __init__(self, val=None):
+        self.left = None
+        self.right = None
+        self.val = val
+
+    def insert(self, val):
+        if not self.val:
+            self.val = val
+            return
+
+        if self.val == val:
+            return
+
+        if val < self.val:
+            if self.left:
+                self.left.insert(val)
+                return
+            self.left = BSTNode(val)
+            return
+
+        if self.right:
+            self.right.insert(val)
+            return
+        self.right = BSTNode(val)

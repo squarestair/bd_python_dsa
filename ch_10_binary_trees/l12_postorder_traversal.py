139498b9affb4d6198035de0928adcf8512fb87a
tw <tw@test.com>
2025-12-01 23:03:08 -0500

    ch10_l12 completed

---
+++ ch_10_binary_trees/l12_postorder_traversal.py
@@ -0,0 +1,36 @@
+class BSTNode:
+    def postorder(self, visited):
+        if self.left is not None:
+            self.left.postorder(visited)
+        if self.right is not None:
+            self.right.postorder(visited)
+        if self.val is not None:
+            visited.append(self.val)
+        return visited
+
+    # don't touch below this line
+
+    def __init__(self, val=None):
+        self.left = None
+        self.right = None
+        self.val = val
+
+    def insert(self, val):
+        if not self.val:
+            self.val = val
+            return
+
+        if self.val == val:
+            return
+
+        if val < self.val:
+            if self.left:
+                self.left.insert(val)
+                return
+            self.left = BSTNode(val)
+            return
+
+        if self.right:
+            self.right.insert(val)
+            return
+        self.right = BSTNode(val)

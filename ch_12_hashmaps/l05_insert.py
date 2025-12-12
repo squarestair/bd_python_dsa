4f519dc93d0122ed9e4c08823a630efadc6228db
squarestair <tim._wong@live.ca>
2025-12-11 23:17:54 -0500

    ch12_l05 completed

---
+++ ch_12_hashmaps/l05_insert.py
@@ -0,0 +1,25 @@
+class HashMap:
+    def insert(self, key, value):
+        ind = self.key_to_index(key)
+        self.hashmap[ind] = (key, value)
+        return
+
+    # don't touch below this line
+
+    def __init__(self, size):
+        self.hashmap = [None for i in range(size)]
+
+    def key_to_index(self, key):
+        sum = 0
+        for c in key:
+            sum += ord(c)
+        return sum % len(self.hashmap)
+
+    def __repr__(self):
+        final = ""
+        for i, v in enumerate(self.hashmap):
+            if v != None:
+                final += f" - {i}: {str(v)}\n"
+            else:
+                final += f" - {i}: None\n"
+        return final

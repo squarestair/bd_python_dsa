7355c063b9cf3b12d1e876efb649416737c6aebc
squarestair <tim._wong@live.ca>
2025-12-11 23:33:10 -0500

    ch12_l06 completed

---
+++ ch_12_hashmaps/l06_get.py
@@ -0,0 +1,30 @@
+class HashMap:
+    def get(self, key):
+        ind = self.key_to_index(key)
+        t = self.hashmap[ind]
+        if t is not None:
+            return t[1]
+        else:
+            raise Exception("sorry, key not found")
+
+    # don't touch below this line
+
+    def __init__(self, size):
+        self.hashmap = [None for i in range(size)]
+
+    def key_to_index(self, key):
+        sum = 0
+        for c in key:
+            sum += ord(c)
+        return sum % len(self.hashmap)
+
+    def insert(self, key, value):
+        i = self.key_to_index(key)
+        self.hashmap[i] = (key, value)
+
+    def __repr__(self):
+        final = ""
+        for i, v in enumerate(self.hashmap):
+            if v != None:
+                final += f" - {str(v)}\n"
+        return final

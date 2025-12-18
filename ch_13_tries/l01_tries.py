6a3f39e1ed828c626008915b68afb222fc49b9fe
squarestair <tim._wong@live.ca>
2025-12-17 23:27:45 -0500

    ch13_l01 fix

--- ch_13_tries/l01_tries.py
+++ ch_13_tries/l01_tries.py
@@ -1,15 +1,11 @@
 class Trie:
     def add(self, word):
         cl = self.root
-        for i, c in enumerate(word):
+        for c in word:
             if c not in cl:
                 cl[c] = {}
             cl = cl[c]
-            if i == len(word) - 1:
-                # print("last char:", c)
-                cl[self.end_symbol] = True
-        # print(f"CL = {cl}")
-        # print("end")
+        cl[self.end_symbol] = True
 
     # don't touch below this line
 
